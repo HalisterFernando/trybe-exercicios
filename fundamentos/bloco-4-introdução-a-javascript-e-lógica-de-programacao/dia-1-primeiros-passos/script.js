2e77470d7b16f2d6f513226952ac3cc07094aa2a
Seu nome <halisterfernando@hotmail.com>
2021-11-26 18:59:46 -0300

    operadores matemáticos

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
@@ -0,0 +1,34 @@
+// Adição 
+
+let sum1 = 5;
+let sum2 = 6;
+let soma = sum1 + sum2;
+console.log(soma)
+
+// Subtração 
+
+let sub1 = 5;
+let sub2 = 3;
+let subt = sub1 - sub2;
+console.log(subt)
+
+// Multiplicação
+
+let x1 = 7;
+let x2 = 8;
+let multi = x1 * x2;
+console.log(multi)
+
+// Divisão
+
+let div1 = 10;
+let div2 = 8;
+let division = div1 / div2;
+console.log(division)
+
+// Módulo 
+
+let rest1 = 5;
+let rest2 = 2;
+let resto = rest1 % rest2;
+console.log(resto)
