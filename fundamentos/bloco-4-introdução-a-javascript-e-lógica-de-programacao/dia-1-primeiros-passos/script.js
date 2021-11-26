b365a136ad592c36de0cc208c5f81912ef4b65b4
Seu nome <halisterfernando@hotmail.com>
2021-11-26 19:10:41 -0300

    retornar o maior dos 3 números

--- fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
@@ -1,4 +1,9 @@
-const alfa = 8;
-const beta = 5;
+// Retorna o maior dos 3 números
 
-if(alfa > beta){console.log(alfa)}
+const alfa = 9;
+const beta = 22;
+const gama = 40;
+
+if(alfa > beta && alfa > gama){console.log(alfa)}
+if(beta > alfa && beta > gama){console.log(beta)}
+if(gama > alfa && gama > beta){console.log(gama)}
