4dd1cd28343c571a5860b198a4a61aa2aa96078b
Seu nome <halisterfernando@hotmail.com>
2021-11-29 11:51:36 -0300

    números ímpares

--- fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
@@ -1,11 +1,12 @@
-// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+//Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
 //Bonus: use somente um if .
 
-const num1 = 22;
-const num2 = 3;
-const num3 = 77;
 
-if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){console.log(true)}else{console.log("false")}
+const num1 = 21;
+const num2 = 3;
+const num3 = 40;
+
+if(num1 % 2 > 0 || num2 % 2 > 0 || num3 % 2 > 0){console.log(true)}else{console.log("false")}
 
 
 
