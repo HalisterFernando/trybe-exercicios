95f98ca73cd55ff63828d72f34d890ff91fb595c
Seu nome <halisterfernando@hotmail.com>
2021-11-29 11:49:22 -0300

    números pares

--- fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
@@ -1,21 +1,11 @@
-/*Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
-Porcentagem >= 90 -> A
-Porcentagem >= 80 -> B
-Porcentagem >= 70 -> C
-Porcentagem >= 60 -> D
-Porcentagem >= 50 -> E
-Porcentagem < 50 -> F
-O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.*/
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+//Bonus: use somente um if .
 
-let nota = 0;
+const num1 = 22;
+const num2 = 3;
+const num3 = 77;
 
-if(nota < 0 || nota > 100){console.log("Nota inválida")}else
-if(nota >= 90){console.log("A")}else
-if(nota >= 80){console.log("B")}else
-if(nota >= 70){console.log("C")}else
-if(nota >= 60){console.log("D")}else
-if(nota >= 50){console.log("E")}else
-if(nota < 50){console.log("F")};
+if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){console.log(true)}else{console.log("false")}
 
 
 
