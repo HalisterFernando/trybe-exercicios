88e2b8e1bff19c60f9e9c465d156c001529fe566
Seu nome <halisterfernando@hotmail.com>
2021-11-29 13:48:18 -0300

    cálculo salário líquido

--- fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
@@ -1,25 +1,51 @@
-//Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+//Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
 
-//Atente que, sobre o custo do produto, incide um imposto de 20%.
+//A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
 
-//Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+//INSS (Instituto Nacional do Seguro Social)
+//Salário bruto até R$ 1.556,94: alíquota de 8%
+//Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+//Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+//Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
 
-//O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+//IR (Imposto de Renda)
+//Até R$ 1.903,98: isento de imposto de renda
+//De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+//De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+//De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+//Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
 
-//valorCustoTotal = valorCusto + impostoSobreOCusto
-//lucro = valorVenda - valorCustoTotal (lucro de um produto)
+const salarioBruto = 3000;
 
-const custo = -1;
-const venda = 30;
-const custoTotal = custo * 0.20 + custo
-const lucro = venda - custoTotal
+const inss1 = salarioBruto * 0.08;
+const inss2 = salarioBruto * 0.09;
+const inss3 = salarioBruto * 0.11;
+const inss4 = 570.80;
 
-if(custo < 0 || venda < 0){console.log("Erro")}else{console.log(lucro)}
+let salarioBase;
 
+if(salarioBruto <= 1556.94){salarioBase = salarioBruto - inss1}else 
+if(salarioBruto >= 1556.95 && salarioBruto <= 2594.92){salarioBase = salarioBruto - inss2}else 
+if(salarioBruto >= 2594.93 && salarioBruto <= 5189.82){salarioBase = salarioBruto - inss3}else 
+if(salarioBruto > 5189.82){salarioBase = salarioBruto - inss4};
 
+console.log(salarioBase)
 
+const ir1 = salarioBase;
+const ir2 = salarioBase * 0.075 - 142.80;
+const ir3 = salarioBase * 0.15 - 354.80;
+const ir4 = salarioBase * 0.225 - 636.13;
+const ir5 = salarioBase * 0.275 - 869.36;
 
+let salarioLiquido;
 
+if(salarioBase <= 1903.98){salarioLiquido = salarioBase}else
+if(salarioBase >= 1903.99 && salarioBase <= 2826.65){salarioLiquido = salarioBase - ir2}else 
+if(salarioBase >= 2826.66 && salarioBase <= 3751.05){salarioLiquido = salarioBase - ir3}else 
+if(salarioBase >= 3751.06 && salarioBase <= 4664.68){salarioLiquido = salarioBase - ir4}else 
+if(salarioBase > 4664.68){salarioLiquido = salarioBase - ir5}
+
+console.log(salarioLiquido)
 
 
 
