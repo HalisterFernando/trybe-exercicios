acaebd1c984f43c510917c768b41e75365458fc9
Seu nome <halisterfernando@hotmail.com>
2021-11-29 12:02:51 -0300

    cálculo de lucro

--- fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-1-primeiros-passos/script.js
@@ -1,12 +1,22 @@
-//Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
-//Bonus: use somente um if .
+//Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+
+//Atente que, sobre o custo do produto, incide um imposto de 20%.
+
+//Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+//O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+
+//valorCustoTotal = valorCusto + impostoSobreOCusto
+//lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+const custo = -1;
+const venda = 30;
+const custoTotal = custo * 0.20 + custo
+const lucro = venda - custoTotal
+
+if(custo < 0 || venda < 0){console.log("Erro")}else{console.log(lucro)}
 
 
-const num1 = 21;
-const num2 = 3;
-const num3 = 40;
-
-if(num1 % 2 > 0 || num2 % 2 > 0 || num3 % 2 > 0){console.log(true)}else{console.log("false")}
 
 
 
