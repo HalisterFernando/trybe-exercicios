a61763934784812e745064818dfa65b40d6dc180
Seu nome <halisterfernando@hotmail.com>
2021-11-29 15:46:51 -0300

    exercicio de loop for

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/script.js
@@ -1,23 +1,8 @@
-// Exercício 1
-// Obtenha o valor "Serviços" do array menu :
+//Exercício 1
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-console.log(menuServices);
-
-
-//Exercício 2
-//Procure o índice do valor "Portfólio" do array menu :
-
-let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu2.indexOf('Portfólio');
-
-console.log(indexOfPortfolio);
-
-//Exercício 3
-//Adicione o valor "Contato" no final do array menu :
-
-let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu3.push("Contato")
-console.log(menu3);
+for(let i = 0; i < groceryList.length; i++){
+    console.log(groceryList[i])
+}
