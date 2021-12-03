95e262ab6e40f9b72021142410db9973982ff80c
Seu nome <halisterfernando@hotmail.com>
2021-12-03 16:04:01 -0300

    exercicios de seletores

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
@@ -0,0 +1,10 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+document.getElementById('page-title').innerText = 'Fragmentado' 
+
+const secondParagraph = document.getElementById('second-paragraph');
+
+secondParagraph.style.color = 'blue';
+
+document.getElementById('subtitle').innerText = 'testando'
+
