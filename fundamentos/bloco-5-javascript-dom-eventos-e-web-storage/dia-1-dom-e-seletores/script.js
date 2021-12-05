fc81fc95f59fba0232291f7b6e1e2ead420776f3
Seu nome <halisterfernando@hotmail.com>
2021-12-05 20:05:43 -0300

    seletores de elementos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
@@ -6,5 +6,7 @@ const secondParagraph = document.getElementById('second-paragraph');
 
 secondParagraph.style.color = 'blue';
 
-document.getElementById('subtitle').innerText = 'testando'
+document.getElementById('subtitle').innerText = 'testando';
+document.getElementsByClassName('header-p')[0].style.color = 'green';
 
+document.getElementsByTagName('h4')[0].style.color = "goldenrod"
