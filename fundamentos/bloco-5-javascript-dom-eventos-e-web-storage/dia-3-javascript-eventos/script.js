9b0859c754d1f759e5e5a501bf59df4e184d53fb
Seu nome <halisterfernando@hotmail.com>
2021-12-08 18:55:11 -0300

    last commit

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -100,25 +100,24 @@ for(let i = 0; i < monthDays.length; i++){
         days.style.fontSize = "20px"
     })
 }
-
+//=================================================================================================================
 /* Exercício 7:
 Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
 O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
  */
-
-
 
 /*Exercício 8:
 Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
 O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
 O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
  */
-
-
-
 /* Exercício 9:
 Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
 Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada. */
+
+//===============================================================================================================
+
+/* Os exercícios 7, 8 e 9 foram unificados no Bônus 
 
 /* Exercício 10:Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
 Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119)  */
@@ -146,6 +145,7 @@ taskInput.addEventListener('keyup', function(event){
     event.preventDefault();
     if(event.keyCode === 13){addBtn.click()}
   })
+  
 addBtn.addEventListener('click', newTask)
 
 function newTask(){
